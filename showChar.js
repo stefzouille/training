7a271c6db6f9270722977ef5c4d58037d5cd5327
stefzouille <contact.lokcy@gmail.com>
2021-02-28 00:03:54 +0100

    add

---
+++ showChar.js
@@ -0,0 +1,8 @@
+//Ecrivez un programme qui affiche ligne par ligne, l'index et le caractère situé à cet index de la phrase suivante:
+
+let text = 'était à Mégara, faubourg de Carthage, dans les jardins d\'Hamilcar.'
+
+for ( i = 0; i < text.length; i++){
+
+  console.log(`le caractere: ${text[i]} a lindex ${[i]}`)
+}
