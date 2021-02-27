d12c6ceabef7b3f901f6c15576682548388d6f0b
stefzouille <contact.lokcy@gmail.com>
2021-02-27 19:12:52 +0100

    add

--- junk.js
+++ junk.js
@@ -1,3 +1,9 @@
-let A = 'b'
-
-console.log(A)
+ const welcome = (f,g) => {
+    let accord = g == "female" ? "e" : "";
+    let msg = "Bonjour "+f+", vous êtes entré" + accord + " sur le chan";
+    return msg;
+}  
+let firstname="bob";
+let gender = "male" ; 
+let msg = welcome(firstname,gender);
+console.log(msg) ;
