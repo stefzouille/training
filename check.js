6f7f9142cf9f09491d7345f260daa79d35238527
stefzouille <contact.lokcy@gmail.com>
2021-02-28 17:21:55 +0100

    add

---
+++ check.js
@@ -0,0 +1,14 @@
+//Vérifier si dans le texte suivant le mot Javascript est présent:
+
+let text = "JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript"
+
+let msg = 'Javascript'
+
+if (text.includes(msg)){
+  console.log(`il y a le mot ${msg} est dans le texte`)  
+  }  else { 
+    console.log(`le mot ${msg} n est pas pas dans le text`)
+}   
+
+  
+  
