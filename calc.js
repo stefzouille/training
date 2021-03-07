63c3f6902dd3271c1f1d055d8ef991ec11f60da3
stefzouille <contact.lokcy@gmail.com>
2021-03-07 17:29:56 +0100

    re

--- calc.js
+++ calc.js
@@ -1,20 +1,20 @@
 // add sub mul div
 
-const add = (n1, n2) => {
-  return n1 + n2
-}
+// const add = (n1, n2) => {
+//   return n1 + n2
+// }
 
-const sub = (n1, n2) => {
-  return n1 - n2
-}
+// const sub = (n1, n2) => {
+//   return n1 - n2
+// }
 
-const mul = (n1, n2) => {
-  return n1 * n2
-}
+// const mul = (n1, n2) => {
+//   return n1 * n2
+// }
 
-const div = (n1, n2) => {
-  return n1 / n2
-}
+// const div = (n1, n2) => {
+//   return n1 / n2
+// }
 
 // const calc = (op, n1, n2) => {
 //   switch (op) {
@@ -31,18 +31,104 @@ const div = (n1, n2) => {
 //   }
 // }
 
-const calc = (op, n1, n2) => {
-  switch(op) {
-    case '+': return add(n1, n2)
-    break
-    case '-': return sub(n1, n2)
-    break
-    case '*': return mult(n1, n2)
-    break
-    case '/': return div(n1, n2)
-    break
-    default : 
-    return 'not good operator,please retry or not.'
-  }
+// const calc = (op, n1, n2) => {
+//   switch(op) {
+//     case '+': return add(n1, n2)
+//     break
+//     case '-': return sub(n1, n2)
+//     break
+//     case '*': return mult(n1, n2)
+//     break
+//     case '/': return div(n1, n2)
+//     break
+//     default : 
+//     return 'not good operator,please retry or not.'
+//   }
+// }
+
+// const add = (n1,n2) => {
+//   return n1 + n2
+// }
+// const sub = (n1,n2) => {
+//   return n1 - n2
+// }
+// const mul = (n1,n2) => {
+//   return n1 * n2
+// }
+// const div = (n1,n2) => {
+//   return n1 / n2
+// }
+// const calc = (op, n1, n2) => {
+//   switch(op) {
+//     case '+': return add(n1,n2)
+//     break
+//     case '-': return sub(n1,n2)
+//     break
+//     case '*': return mul(n1,n2)
+//     break
+//     case '/': return div(n1,n2)
+//     break
+//     default :
+//     return 'your fck operante is no good man,replace and retry, or.....'
+//   }
+// }
+// const add = (n1, n2) => {
+//   return n1 + n2
+// }
+
+// const sub = (n1,n2) => {
+//   return n1 - n2
+// }
+
+// const mul = (n1,n2) => {
+//   return n1 * n2
+// }
+
+// const div = (n1,n2) => {
+//   return n1 / n2
+// }
+
+// const calc = (op,n1,n2) => {
+// switch (op) {
+//   case '+': return add(n1,n2)
+//   break
+//   case '-': return sub(n1,n2)
+//   break
+//   case '*': return mul(n1,n2)
+//   break
+//   case '/': return div(n1,n2)
+//   break
+//   default :
+//   return 'fck op is not good retry now oki !!'
+//  }
+// }
+
+const add = (n1,n2) => {
+  return n1 + n2
 }
+const sub = (n1,n2) => {
+  return n1 - n2
+}
+const mul = (n1,n2) => {
+  return n1 * n2
+}
+const div = (n1,n2) => {
+  return n1 / n2
+}
+const calc = (op,n1,n2) => {
+  switch (op) {
+    case '+': return add(n1,n2)
+    break
+    case '-': return sub(n1,n2)
+    break
+    case '*': return mul(n1,n2)
+    break
+    case '/': return div(n1,n2)
+    break
+    default:
+      return 'your op is not good replace and retry'
+  }
+  
+}
+
 console.log(calc('m', 10, 6))
