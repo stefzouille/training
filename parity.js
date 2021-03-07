0207397736103f601dab511ff1969e2068c1b563
stefzouille <contact.lokcy@gmail.com>
2021-03-07 17:45:11 +0100

    rpt

---
+++ parity.js
@@ -0,0 +1,19 @@
+// Ecrire une fonction isOdd qui prend un nombre en paramètre et qui devra retourner true si le nombre passé en paramètre est impair, 
+// sinon la fonction retournera false.
+
+// const isOdd = (nb) => {
+//   return nb % 2 !== 0 ? true : false
+// }
+
+// const isOdd = (nb) => {
+//   return nb % 2 !== 0 ? true : false
+// }
+// const isOdd = (nb) => {
+//   return nb % 2 !== 0
+// }
+
+const isOdd = (nb) => {
+  return nb % 2 !== 0 ? true : false
+}
+
+console.log(isOdd(32))
---
+++ typeOf.js
@@ -0,0 +1,14 @@
+// Ecrire une fonction typeOf qui prend comme paramètre une variable, et qui retourne une string qui sera le nom du type de cette variable passée en paramètre. Vous devrez utiliser l'opérateur typeof pour cela
+
+// const typeOf = (wat) => {
+//   return typeof wat
+// }
+// const typeOf = (vv) => {
+//   return typeof vv
+// }
+
+const typeOf = (fr) => {
+  return typeof fr
+}
+
+console.log(typeOf(2))
