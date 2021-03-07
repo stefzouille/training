dd7da4c4f79b7ea14a25e69e1e98e51439bf4020
stefzouille <contact.lokcy@gmail.com>
2021-03-07 17:11:36 +0100

    rpt

---
+++ count.js
@@ -0,0 +1,43 @@
+// Ecrire une fonction count qui prend comme paramètre un nombre min, un nombre max et un nombre step. L'exécution de cette fonction devra afficher sur le terminal tous les nombres de min jusqu'a max avec un intervale de step
+
+// const count = (min, max, step) => {
+//   for ( i = min; i <= max; i += step) {
+//     console.log(i)
+//   }
+// }
+
+// const count = (min, max, step) => {
+//   for ( i = min; i <= max; i += step) {
+//     console.log(i)
+//   }
+//}
+
+// const count = (min, max, step) => {
+//   for (i = min; i <= max; i += step)
+//   console.log(i)
+// }
+// const count = (min, max, step) => {
+//   for (i = min; min <= max; i += step) {
+//     console.log(i)
+//   }
+// }
+
+// const count = (min, max, step) => {
+//   for ( i =min; i <= max; i += step) {
+//     console.log(i)
+//   }
+// }
+
+// const count = (min, max, step) => {
+//   for (i = min; i <= max; i += step) {
+//     console.log(i)
+//   }
+// }
+
+const count = (min, max, step) => {
+  for (i = min; i <= max; i += step) {
+    console.log(i)
+  }
+}
+
+count(10, 100, 10)
