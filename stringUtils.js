8af305fe415259d95a6c9bb696a63d7451732044
stefzouille <contact.lokcy@gmail.com>
2021-03-07 22:20:59 +0100

    RPT

---
+++ stringUtils.js
@@ -0,0 +1,18 @@
+// Ecrire une fonction reverseStr qui prend en paramètre une string et qui retourne cette string inversé. Si le paramètre passé est 'Hello!'  
+// la fonction devra retourné '!olleH'
+
+// const reverseStr = (str) => {
+//   return str.split('').reverse().join('')
+// } 
+
+// const reverseStr = (str) => {
+//   return str.split('').reverse().join('')
+// }
+
+// const reverseStr = (str) => {
+//   return str.split('').reverse().join('')
+// } 
+
+
+
+console.log(reverseStr('hello!'))
